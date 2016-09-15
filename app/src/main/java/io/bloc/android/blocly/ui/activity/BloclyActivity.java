206464d4dbd787804c20da999a8d533aedf8e448
Ben Weinstein <biny99@gmail.com>
2016-09-14 22:47:20 -0400

    assignment 34 completed

--- app/src/main/java/io/bloc/android/blocly/ui/activity/BloclyActivity.java
+++ app/src/main/java/io/bloc/android/blocly/ui/activity/BloclyActivity.java
@@ -2,7 +2,7 @@ package io.bloc.android.blocly.ui.activity;
 
 import android.app.Activity;
 import android.os.Bundle;
-import android.widget.Toast;
+import android.widget.TextView;
 
 import io.bloc.android.blocly.BloclyApplication;
 import io.bloc.android.blocly.R;
@@ -10,12 +10,16 @@ import io.bloc.android.blocly.R;
 /**
  * Created by Ben Weinstein on 9/4/2016.
  */
-public class BloclyActivity extends Activity {
+public class BloclyActivity extends Activity
+{
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_blocly);
-        Toast.makeText(this, BloclyApplication.getSharedDataSource().getFeeds().get(0).getTitle(),Toast.LENGTH_LONG).show();
+        TextView feedText = (TextView) findViewById(R.id.feed_name);
+        feedText.setText(BloclyApplication.getSharedDataSource().getFeeds().get(0).getTitle());
     }
 }
