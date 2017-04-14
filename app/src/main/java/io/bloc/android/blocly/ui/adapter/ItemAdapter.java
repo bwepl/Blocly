42e98a0f9a8da1335cd8d22020ebef37fd2085c2
Ben Weinstein <biny99@gmail.com>
2017-04-13 23:42:56 -0400

    checkpoint 39 started

--- app/src/main/java/io/bloc/android/blocly/ui/adapter/ItemAdapter.java
+++ app/src/main/java/io/bloc/android/blocly/ui/adapter/ItemAdapter.java
@@ -6,6 +6,8 @@ import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.CheckBox;
+import android.widget.CompoundButton;
 import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
@@ -47,13 +49,17 @@ public class ItemAdapter extends RecyclerView.Adapter<ItemAdapter.ItemAdapterVie
         return BloclyApplication.getSharedDataSource().getItems().size();
     }
 
-    class ItemAdapterViewHolder extends RecyclerView.ViewHolder implements ImageLoadingListener, View.OnClickListener
+    class ItemAdapterViewHolder extends RecyclerView.ViewHolder implements ImageLoadingListener, View.OnClickListener, CompoundButton.OnCheckedChangeListener
     {
         TextView title;
         TextView feed;
         TextView content;
         View headerWrapper;
         ImageView headerImage;
+
+        CheckBox archiveCheckbox;
+        CheckBox favoriteCheckbox;
+
         RssItem rssItem;
 
         public ItemAdapterViewHolder(View itemView)
@@ -64,7 +70,12 @@ public class ItemAdapter extends RecyclerView.Adapter<ItemAdapter.ItemAdapterVie
             content = (TextView) itemView.findViewById(R.id.tv_rss_item_content);
             headerWrapper = itemView.findViewById(R.id.fl_rss_item_image_header);
             headerImage = (ImageView) headerWrapper.findViewById(R.id.iv_rss_item_image);
+            archiveCheckbox = (CheckBox) itemView.findViewById(R.id.cb_rss_item_check_mark);
+            favoriteCheckbox = (CheckBox) itemView.findViewById(R.id.cb_rss_item_favorite_star);
             itemView.setOnClickListener(this);
+
+            archiveCheckbox.setOnCheckedChangeListener(this);
+            favoriteCheckbox.setOnCheckedChangeListener(this);
         }
 
         void update(RssFeed rssFeed, RssItem rssItem)
@@ -125,6 +136,12 @@ public class ItemAdapter extends RecyclerView.Adapter<ItemAdapter.ItemAdapterVie
         public void onClick(View view)
         {
             Toast.makeText(view.getContext(), rssItem.getTitle(), Toast.LENGTH_SHORT).show();
+        }
+
+        @Override
+        public void onCheckedChanged(CompoundButton buttonView, boolean isChecked)
+        {
+            Log.v(TAG, "Checked changed to: " + isChecked);
         }
     }
 }
